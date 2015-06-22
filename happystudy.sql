3fbfefdfedb5a38cfa2c1aa2440bc6e3ce3a6552
yaolei <yaolei@chinasofti.com>
2015-06-22 20:39:05 +0800

    补上

--- happystudy.sql
+++ happystudy.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : jdbc:mysql://127.0.0.1:3306/happystudy
+Source Server         : jdbc:mysql://127.0.0.1:3306/data
 Source Server Version : 50151
 Source Host           : 127.0.0.1:3306
 Source Database       : happystudy
@@ -10,10 +10,33 @@ Target Server Type    : MYSQL
 Target Server Version : 50151
 File Encoding         : 65001
 
-Date: 2015-06-17 16:30:31
+Date: 2015-06-22 20:38:14
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `hs_chapter`
+-- ----------------------------
+DROP TABLE IF EXISTS `hs_chapter`;
+CREATE TABLE `hs_chapter` (
+  `CHAPTER_ID` varchar(255) NOT NULL COMMENT '章节ID',
+  `CHAPTER_NAME` varchar(255) DEFAULT NULL COMMENT '章节名称',
+  PRIMARY KEY (`CHAPTER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='章节';
+
+-- ----------------------------
+-- Records of hs_chapter
+-- ----------------------------
+INSERT INTO `hs_chapter` VALUES ('C01', '第一章');
+INSERT INTO `hs_chapter` VALUES ('C02', '第二章');
+INSERT INTO `hs_chapter` VALUES ('C03', '第三章');
+INSERT INTO `hs_chapter` VALUES ('C04', '第四章');
+INSERT INTO `hs_chapter` VALUES ('C05', '第五章');
+INSERT INTO `hs_chapter` VALUES ('C06', '第六章');
+INSERT INTO `hs_chapter` VALUES ('C07', '第七章');
+INSERT INTO `hs_chapter` VALUES ('C08', '第八章');
+INSERT INTO `hs_chapter` VALUES ('C09', '第九章');
+INSERT INTO `hs_chapter` VALUES ('C10', '法律法规');
 
 -- ----------------------------
 -- Table structure for `hs_check`
@@ -31,7 +54,8 @@ CREATE TABLE `hs_check` (
 -- ----------------------------
 -- Records of hs_check
 -- ----------------------------
-INSERT INTO `hs_check` VALUES ('8a82f0d04dfc6e77014dfcb0169d0006', '2015-06-16 22:05:08', '北京市海淀区西北旺镇北清路54号', '签到', '8a82f0d04dfc6e77014dfcaf0f4a0004');
+INSERT INTO `hs_check` VALUES ('402881ed4e0c3559014e0c3876410002', '2015-06-19 22:28:23', '北京市昌平区东小口地区和谐家园(二区)', '签到', '402881ed4e0c3559014e0c3559eb0000');
+INSERT INTO `hs_check` VALUES ('402881ed4e0c3559014e0c3884e90003', '2015-06-19 22:28:27', '北京市昌平区东小口地区和谐家园(二区)', '签到', '402881ed4e0c3559014e0c3559eb0000');
 
 -- ----------------------------
 -- Table structure for `hs_class`
@@ -46,10 +70,28 @@ CREATE TABLE `hs_class` (
 -- ----------------------------
 -- Records of hs_class
 -- ----------------------------
-INSERT INTO `hs_class` VALUES ('8a82f0d04dfaa4b8014dfaa4b8370000', '20150616');
-INSERT INTO `hs_class` VALUES ('8a82f0d04dfaa4b8014dfaa4d4d80001', '20150617');
-INSERT INTO `hs_class` VALUES ('8a82f0d04dfaa4b8014dfaa4eee80002', '20150618');
-INSERT INTO `hs_class` VALUES ('8a82f0d04dfaa4b8014dfaa50bdc0003', '20150619');
+INSERT INTO `hs_class` VALUES ('402881e94e01e801014e01e801470000', '20150615');
+INSERT INTO `hs_class` VALUES ('402881e94e01e801014e01e813260001', '20150616');
+INSERT INTO `hs_class` VALUES ('402881e94e01e801014e01e8233e0002', '20150617');
+INSERT INTO `hs_class` VALUES ('402881e94e01e801014e01e835560003', '20150614');
+INSERT INTO `hs_class` VALUES ('402881e94e01e801014e01e84eb70004', '20150618');
+INSERT INTO `hs_class` VALUES ('402881e94e01e801014e01e86fce0005', '20150619');
+
+-- ----------------------------
+-- Table structure for `hs_collect`
+-- ----------------------------
+DROP TABLE IF EXISTS `hs_collect`;
+CREATE TABLE `hs_collect` (
+  `COLLECT_ID` varchar(255) NOT NULL COMMENT '收藏ID',
+  `TOPIC_ID` varchar(255) DEFAULT NULL COMMENT '题目ID',
+  `USER_ID` varchar(255) DEFAULT NULL COMMENT '用户ID',
+  PRIMARY KEY (`COLLECT_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='收藏';
+
+-- ----------------------------
+-- Records of hs_collect
+-- ----------------------------
+INSERT INTO `hs_collect` VALUES ('402881ed4e1adeef014e1ae62c060001', '402881ed4e0c3559014e0c3c7f760004', '402881ed4e0c3559014e0c3559eb0000');
 
 -- ----------------------------
 -- Table structure for `hs_exam_record`
@@ -85,8 +127,7 @@ CREATE TABLE `hs_opinion` (
 -- ----------------------------
 -- Records of hs_opinion
 -- ----------------------------
-INSERT INTO `hs_opinion` VALUES ('8a82f0cc4dffc5c5014dffc5c5b70000', '我们', 'yaolei@163.com', '2015-06-17 12:27:40', '8a82f0d04dfc6e77014dfcaf0f4a0004');
-INSERT INTO `hs_opinion` VALUES ('8a82f0d04dfc6e77014dfcaf9d350005', '这个应用真好用', '12345678901', '2015-06-16 22:04:37', '8a82f0d04dfc6e77014dfcaf0f4a0004');
+INSERT INTO `hs_opinion` VALUES ('402881ed4e0c3559014e0c35e91b0001', '这个应用很好', '12345678901', '2015-06-19 22:25:36', '402881ed4e0c3559014e0c3559eb0000');
 
 -- ----------------------------
 -- Table structure for `hs_paper`
@@ -114,7 +155,7 @@ CREATE TABLE `hs_paper_topic` (
   `PAPER_ID` varchar(255) DEFAULT NULL COMMENT '试卷ID',
   `TOPIC_ID` varchar(255) DEFAULT NULL COMMENT '题目ID',
   PRIMARY KEY (`PAPER_TOPIC_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='试卷题目';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='题目试卷';
 
 -- ----------------------------
 -- Records of hs_paper_topic
@@ -132,13 +173,21 @@ CREATE TABLE `hs_topic` (
   `TOPIC_SCORE` varchar(255) DEFAULT NULL COMMENT '题目分值',
   `ANALYSIS` varchar(255) DEFAULT NULL COMMENT '详解',
   `DIFFICULTY` varchar(255) DEFAULT NULL COMMENT '难易度',
-  `CHAPTER` varchar(255) DEFAULT NULL COMMENT '所属章节',
+  `CHAPTER_ID` varchar(255) DEFAULT NULL COMMENT '章节ID',
   PRIMARY KEY (`TOPIC_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='题目';
 
 -- ----------------------------
 -- Records of hs_topic
 -- ----------------------------
+INSERT INTO `hs_topic` VALUES ('402881ed4e0c3559014e0c3c7f760004', '一年期的意外伤害保险费率厘定的因素主要是考虑（）', '预定利率##意外的持续时间##被保险人的职业、工种##被保险人的死亡率', 'C', '1', '答案：C ', '1', 'C01');
+INSERT INTO `hs_topic` VALUES ('402881ed4e0c3559014e0c3dbe4f0005', '下列不属于人身保险特征的是（）', '正确##错误', 'C', '1', '答案：C', '1', 'C01');
+INSERT INTO `hs_topic` VALUES ('402881ed4e0c3559014e0c3ee4290006', '在家庭财产保险中，保险事故发生后，保险人对于房屋及室内附属设备、室内装潢的赔偿处理主要采取（）', '第一危险赔偿方式##比例赔偿方式##限额责任制##顺序责任制', 'B', '1', '答案：B', '1', 'C02');
+INSERT INTO `hs_topic` VALUES ('402881ed4e0c3559014e0c3ff41d0007', '下面属于投机风险的是（）', '在期货市场上发生盈亏##房屋发生水灾遭受损失##行人被汽车撞死##庄稼被冰雹破坏', 'A', '1', '答案：A', '1', 'C03');
+INSERT INTO `hs_topic` VALUES ('402881ed4e0c3559014e0c40f2540008', '在人寿保险中，当投保人申请退保时，保险人对保单现金价值的处理是（）', '不予退还##全部退还##部分退还##协商退还', 'B', '1', '答案：B', '1', 'C04');
+INSERT INTO `hs_topic` VALUES ('402881ed4e14f6ad014e14f6adf20000', '财产保险合同是给付性合同', '正确##错误', 'B', '1', '答案：B', '1', 'C02');
+INSERT INTO `hs_topic` VALUES ('402881ed4e14f6ad014e14f750190001', '根据我国《保险法》的规定，保险公司分支机构不具有法人资格，其民事责任由保险公司承担', '人身风险的特殊性##保险标的的特殊性##保险费率厘定的特殊性##保险金额确定的特殊性', 'A', '1', '答案：A', '1', 'C01');
+INSERT INTO `hs_topic` VALUES ('402881ed4e14f6ad014e1500ec780002', '根据我国《保险法》的规定，我国制定《保险法》的目的是：为了规范保险活动，保护保险人的合法权益，加强对保险业的监督管理，促进保险事业的健康发展', '正确##错误', 'A', '1', '答案：B', '1', 'C05');
 
 -- ----------------------------
 -- Table structure for `hs_topic_record`
@@ -151,6 +200,7 @@ CREATE TABLE `hs_topic_record` (
   `IS_RIGHT` varchar(255) DEFAULT NULL COMMENT '是否正确',
   `TOPIC_ID` varchar(255) DEFAULT NULL COMMENT '题目ID',
   `EXAM_RECORD_ID` varchar(255) DEFAULT NULL COMMENT '考试记录ID',
+  `USER_ID` varchar(255) DEFAULT NULL COMMENT '用户ID',
   PRIMARY KEY (`TOPIC_RECORD_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='做题记录';
 
@@ -175,27 +225,10 @@ CREATE TABLE `hs_user` (
   `TEAM_TYPE` varchar(255) DEFAULT NULL COMMENT '班型',
   `ROLE` varchar(255) DEFAULT NULL COMMENT '角色',
   `TEAM_CLASS` varchar(255) DEFAULT NULL COMMENT '班级',
-  PRIMARY KEY (`USER_ID`),
-  UNIQUE KEY `USER_PHONE_NO` (`PHONE_NO`)
+  PRIMARY KEY (`USER_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户';
 
 -- ----------------------------
 -- Records of hs_user
 -- ----------------------------
-INSERT INTO `hs_user` VALUES ('8a82f0d04dfc6e77014dfcaf0f4a0004', '15801611227', 'q', '姚磊', '女', '123456789012345', 'q1', '12345678901', '合肥1', '职前班', '普通用户', '20150616');
-
--- ----------------------------
--- Table structure for `hs_user_topic`
--- ----------------------------
-DROP TABLE IF EXISTS `hs_user_topic`;
-CREATE TABLE `hs_user_topic` (
-  `USER_TOPIC_ID` varchar(255) NOT NULL COMMENT '用户题目ID',
-  `COLLECT` varchar(255) DEFAULT NULL COMMENT '标记收藏',
-  `TOPIC_ID` varchar(255) DEFAULT NULL COMMENT '题目ID',
-  `USER_ID` varchar(255) DEFAULT NULL COMMENT '用户ID',
-  PRIMARY KEY (`USER_TOPIC_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户题目';
-
--- ----------------------------
--- Records of hs_user_topic
--- ----------------------------
+INSERT INTO `hs_user` VALUES ('402881ed4e0c3559014e0c3559eb0000', '15801611227', 'q', '姚磊', '女', '123456789012345678', '123', '12345678901', '合肥1', '导师技能提升班', '普通用户', '20150615');
